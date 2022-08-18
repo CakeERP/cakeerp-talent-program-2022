0a3c719156513d8c9f0e82c33481765183ca2900
joaopdadv <joaopdadv@gmail.com>
2022-08-18 15:24:33 -0300

    Atividades aula 3

--- code/day_3/api.py
+++ code/day_3/api.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    age: int
+    male: bool
+
 
 @app.get("/{item_id}")
 async def root(item_id: int):
@@ -14,4 +21,16 @@ async def root():
         "message": "Hello World",
         "contagem": 24,
         "lista": [3,4,5,6,"teste", False],
+    }
+
+@app.post("/items")
+async def create_item(item: Item):
+    return{
+        item
+    }
+
+@app.get("/status")
+async def root():
+    return{
+        "status": "it's alive",
     }
---
+++ code/day_3/models.py
@@ -0,0 +1,15 @@
+import imp
+from operator import index
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, index=True)
+    description = Column(String, index=True)
+
