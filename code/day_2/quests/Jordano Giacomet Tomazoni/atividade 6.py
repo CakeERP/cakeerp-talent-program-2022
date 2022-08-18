84e06d2375d3e83ccecef7d7698237396dbde39d
Jordano <JGTomazoni@ucs.br>
2022-08-18 00:58:40 -0300

    Atividade 6 - Resposta

---
+++ code/day_2/quests/Jordano Giacomet Tomazoni/atividade 6.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Digite o primeiro número: \n"))
+numero2 = int(input("Digite o segundo número: \n"))
+
+if numero1 > numero2:
+    print(f'O maior número é {numero1}')
+else:
+    print(f'O maior número é {numero2}')
